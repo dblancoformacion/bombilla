bb0e1a3c51fdf9f6cfa553c38e309a6c5efa75e1
dblancoformacion <dblancoformacion@gmail.com>
2019-06-14 16:03:19 +0200

    Solución orientada a objetos (POO)

--- index.php
+++ index.php
@@ -1,20 +1,25 @@
 <?php
+Class Bombilla{
+	// vista
+	function on(){
+		return '
+			<a href="?off=1">
+				<img src="bombilla_on.jpg" style="height:200px"/>
+			</a>
+		';
+	}
+	function off(){
+		return '
+			<a href="?on=1">
+				<img src="bombilla_off.jpg" style="height:200px"/>
+			<a/>
+		';
+	}	
+}
 // controlador
-if( isset($_GET['on']) ) echo on();
-else echo off();
-
-function on(){
-	return '
-		<a href="?off=1">
-			<img src="bombilla_on.jpg" style="height:200px"/>
-		</a>
-	';
-}
-function off(){
-	return '
-		<a href="?on=1">
-			<img src="bombilla_off.jpg" style="height:200px"/>
-		<a/>
-	';
-}
+$b=new Bombilla();
+if( isset($_GET['on']) )
+	echo $b->on();
+else 
+	echo $b->off();
 ?>
