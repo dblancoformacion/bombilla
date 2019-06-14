3d0a9200709c9b25d3a4d2dec8b4f59d711c6406
dblancoformacion <dblancoformacion@gmail.com>
2019-06-14 15:36:49 +0200

    Solución procedimental

--- index.php
+++ index.php
@@ -1,13 +1,20 @@
 <?php
 // controlador
-if( isset($_GET['on']) ) echo '
-	<a href="?off=1">
-		<img src="bombilla_on.jpg" style="height:200px"/>
-	</a>
-';
-else echo '
-	<a href="?on=1">
-		<img src="bombilla_off.jpg" style="height:200px"/>
-	<a/>
-';
+if( isset($_GET['on']) ) echo on();
+else echo off();
+
+function on(){
+	return '
+		<a href="?off=1">
+			<img src="bombilla_on.jpg" style="height:200px"/>
+		</a>
+	';
+}
+function off(){
+	return '
+		<a href="?on=1">
+			<img src="bombilla_off.jpg" style="height:200px"/>
+		<a/>
+	';
+}
 ?>
