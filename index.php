bfc0dfc5476e9d86dc0928847b74b96237a31f72
dblancoformacion <dblancoformacion@gmail.com>
2019-06-14 19:30:13 +0200

    Solución con 5 bombillas independienes, persistencia en la URL

--- index.php
+++ index.php
@@ -1,33 +1,43 @@
 <?php
 Class Bombilla{
-	public $id;
-	public $estado=0;
+	public $estado;
 	// modelo
-	function __construct($i){
-		$this->id=$i;
+	function __construct($n){
+		for($i=0;$i<$n;$i++){
+			$this->estado[$i]='off';
+			if(isset($_GET['estados'][$i]))
+				$this->estado[$i]=$_GET['estados'][$i];
+		}
 	}
-	// vista
-	function on(){
-		return '
-			<a href="?off=1">
-				<img src="bombilla_on.jpg" style="height:200px"/>
-			</a>
-		';
-	}
-	function off(){
-		return '
-			<a href="?on=1">
-				<img src="bombilla_off.jpg" style="height:200px"/>
-			<a/>
-		';
+	function cambia(){
+		foreach($this->estado as $i=>$estado){
+			if($_GET['cambia']==$i){
+				if($estado=='off')
+					$this->estado[$i]='on';
+				else $this->estado[$i]='off';
+			}
+		}
 	}	
+	// vista
+	function muestra(){
+		$txt=null;
+		$url=null;
+		foreach($this->estado as $i=>$estado){
+			$url.='&estados[]='.$estado;
+		}
+		foreach($this->estado as $i=>$estado){
+			$txt.='
+				<a href="?cambia='.$i.$url.'">
+					<img src="bombilla_'.$estado.'.jpg" style="height:200px"/>
+				</a>
+			';
+		}			
+		return $txt;
+	}
 }
 // controlador
-for($i=0;$i<5;$i++){
-	$b[$i]=new Bombilla($i);
-	if( isset($_GET['on']) )
-		echo $b[$i]->on();
-	else
-		echo $b[$i]->off();
-}
+$b=new Bombilla(5);
+if( isset($_GET['cambia']) )
+	$b->cambia();
+echo $b->muestra();
 ?>
